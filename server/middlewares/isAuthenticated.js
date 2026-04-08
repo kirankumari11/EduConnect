465dffca7d825487074efccd78e06122838f6f0f
Kiran <itskiran119@gmail.com>
2026-04-08 14:23:12 +0530

    project uploaded

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -1,20 +1,14 @@
-// import jwt from "jsonwebtoken";
 import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
   try {
-    // Safely get token from cookies or Bearer header
     let token = null;
-
-    // Check cookies
     if (req.cookies && req.cookies.token) {
       token = req.cookies.token;
     } 
-    // Check Authorization header (Bearer token)
     else if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
       token = req.headers.authorization.split(" ")[1];
     }
-
     if (!token) {
       return res.status(401).json({
         message: "User not authenticated",
